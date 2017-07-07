fb64eafb4149040daff31c5843517a28b7d26205
phoSk <bestkangaskhaneuw@gmail.com>
2017-07-07 11:58:51 +0200

    Sam liandola

---
+++ CODE/CPTC/Assets/Scripts/Easings/EsingPanel.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EsingPanel : MonoBehaviour {
+
+    public Vector2 initValue;
+    public float framesDuration;
+    public float framesDuration2;
+    public float framesCounter;
+    public float framesCounter2;
+    public Image myImage;
+    public GameObject finalPosition;
+    public GameObject startPosition;
+    public AnimationCurve hyb;
+    public bool active;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        myImage = GetComponent<Image>();
+        initValue = myImage.rectTransform.position;
+        framesCounter = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+
+            active = !active;
+
+            framesCounter = 0;
+            framesCounter2 = 0;
+
+        }
+
+        if (active)
+        {
+
+            framesCounter += Time.deltaTime;
+
+            myImage.rectTransform.position = Vector2.Lerp(initValue, finalPosition.transform.position, hyb.Evaluate(framesCounter / framesDuration));
+
+            if (framesCounter >= framesDuration)
+            {
+
+                framesCounter = framesDuration;
+
+            }
+
+        }
+
+        if (!active)
+        {
+
+            framesCounter2 += Time.deltaTime;
+
+            myImage.rectTransform.position = Vector2.Lerp(myImage.rectTransform.position, startPosition.transform.position, hyb.Evaluate(framesCounter2 / framesDuration2));
+
+            if (framesCounter2 >= framesDuration2)
+            {
+
+                framesCounter2 = framesDuration2;
+
+            }
+
+        }
+        
+    }
+}
---
+++ CODE/CPTC/Assets/Scripts/Easings/Rename.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Rename : MonoBehaviour {
+
+    private Vector2 initValue;
+    public float framesDuration;
+    private float framesCounter;
+    private Image myImage;
+    public GameObject finalPosition;
+    public AnimationCurve hyb;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        myImage = GetComponent<Image>();
+        initValue = myImage.rectTransform.position;
+        framesCounter = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+            framesCounter += Time.deltaTime;
+
+            myImage.rectTransform.position = Vector2.Lerp(initValue, finalPosition.transform.position, hyb.Evaluate(framesCounter / framesDuration));
+
+            if(framesCounter >= framesDuration)
+            {
+
+                framesCounter = framesDuration;
+
+            }
+    }
+}
+
